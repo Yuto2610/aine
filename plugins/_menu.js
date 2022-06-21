a668649e2cdd6f6823d2337031e7eb56bc3e92f4
cekilv <107114396+cekilv@users.noreply.github.com>
2022-06-21 17:55:26 +0000

    Add files via upload

---
+++ plugins/_menu.js
@@ -0,0 +1,11 @@
+/*let fs = require('fs')
+let handler = async (m) => {
+let menu = fs.readFileSync('./mp3/jancok.mp3')
+conn.sendFile(m.chat, menu, '', '', m, true)
+}
+
+handler.customPrefix = /^(bot|halo|wibu|menu|me\nu|.m\enu)$/i
+handler.command = new RegExp
+handler.limit = true
+
+module.exports = handler*/
